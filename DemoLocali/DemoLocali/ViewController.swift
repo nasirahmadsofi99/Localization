0b1ce02076ebc9d87cf8b0e3e4db9a8450f20916
nasirahmadsofi <53961844+nasirahmadsofi99@users.noreply.github.com>
2020-03-31 09:58:28 +0530

    Locali

---
+++ DemoLocali/DemoLocali/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DemoLocali
+//
+//  Created by Deftsoft on 06/03/20.
+//  Copyright © 2020 Deftsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        let buttonTitle = NSLocalizedString("bear", comment: "The name of the animal")
+//        let buttonTitle = NSLocalizedString("Button", comment: "")
+//        button.titleLabel?.text = buttonTitle
+        
+        
+    }
+
+
+}
+
